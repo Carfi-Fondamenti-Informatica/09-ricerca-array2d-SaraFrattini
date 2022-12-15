e0e00a1b84d68b9b06a1357eaecaaf9d2f963808
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-12-15 22:39:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char nomi [10][20] ;
+    char proprio[20] ;
+    int p=1;
+    for(int i=0; i<10; i++){
+        for(int k=0; k<20; k++){
+            nomi[i][k]=' ';
+            proprio[k]=' ';
+        }
+    }
+    for(int i=0; i<10; i++){
+        cin >> nomi[i];
+    }
+    cin>>proprio;
+    if(trovNome(proprio, nomi, p)){
+        cout << p << endl;
+    }else{
+        cout << "non presente" << endl;
+    }
   return 0;
 }
