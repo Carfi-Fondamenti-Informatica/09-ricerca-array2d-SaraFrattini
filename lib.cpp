dc51160fd5baa617f1243b1017c11f3c074f453c
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-12-15 22:37:24 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool trovNome (char proprio[20] , char nomi[10][20] , int &p){
+    for(int i=0; i<10; i++){
+        for(int k=0; k<20; k++){
+            if(proprio[k] != nomi[i][k]){
+                k=20;
+            }else if(k==19){
+                p=i;
+                return true;
+            }
+        }
+    }return false;
+}
